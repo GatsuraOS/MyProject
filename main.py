34e33cba599c0ede2b6707afc704b36a011ce763
GatsuraOS <liolic_88@mail.ru>
2022-07-06 14:39:55 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-N = int(input("N="))
-my_lst = [i for i in range(1, N + 1)]
-dictinary = dict.fromkeys(my_lst, [{"name": input("name "), "email": input("email ")}])
+n = int(input("Введите n "))
 
-print(dictinary)
+list_a: list = [1]
+print(list_a)
+
+if n != 0:
+    for i in range(n+1):
+        list_b: list = list_a.copy()
+        list_a.append(1)
+        # print(list_a)
+        # print(list_b)
+        for j in range(i):
+            list_a[j + 1] = list_b[j] + list_b[j + 1]
+        print(list_a)
