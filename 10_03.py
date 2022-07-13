baefc5247ed9de1dced4e579ff8dcdb6bc594b3a
GatsuraOS <liolic_88@mail.ru>
2022-07-13 21:10:48 +0300

    update

--- 10_03.py
+++ 10_03.py
@@ -3,8 +3,6 @@ with (
     open("output.txt", "w", encoding="utf-8") as file2
 ):
     for line in file:
-        lines = line.strip()
-        words = lines.count(" ") + 1
-        file2.write(str(words) + "\n")
-
+        lines = line.split()
+        file2.write(str(len(lines)) + "\n")
 
