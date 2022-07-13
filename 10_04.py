d4413985ffcd2c113433c5f27cf0e8fd9b526509
GatsuraOS <liolic_88@mail.ru>
2022-07-13 21:28:59 +0300

    initial commit

---
+++ 10_04.py
@@ -0,0 +1,8 @@
+with (
+    open ("input.txt", "r", encoding="utf-8") as file,
+    open ("output.txt", "w", encoding="utf-8") as file2
+):
+    for line in file:
+        line = line.strip()
+        line = list(reversed(line))
+        file2.write("".join(line) + "\n")
