067b1a7afada383c9789e958ab7a6e0965376cd2
GatsuraOS <liolic_88@mail.ru>
2022-07-13 13:49:22 +0300

    update

--- 10_01.py
+++ 10_01.py
@@ -1,8 +1,15 @@
+import math
+
+
 n = int(input("N: "))
 
 
 with open ("input.txt", "r", encoding="utf-8") as file:
     lines = [line.strip() for line in file]
+
+
+if len(lines) < n:
+    lines *= math.ceil(n // len(lines))
 
 
 for i in range(n):
