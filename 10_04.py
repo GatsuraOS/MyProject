3fa60585584260c3887485d69e9edc6d7b13b9e9
GatsuraOS <liolic_88@mail.ru>
2022-07-13 21:29:25 +0300

    update

--- 10_04.py
+++ 10_04.py
@@ -1,6 +1,6 @@
 with (
-    open ("input.txt", "r", encoding="utf-8") as file,
-    open ("output.txt", "w", encoding="utf-8") as file2
+    open("input.txt", "r", encoding="utf-8") as file,
+    open("output.txt", "w", encoding="utf-8") as file2
 ):
     for line in file:
         line = line.strip()
