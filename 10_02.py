59cb8bb6bb58612bdcb2d184346e2c1d5d333f48
GatsuraOS <liolic_88@mail.ru>
2022-07-13 16:51:42 +0300

    update

--- 10_02.py
+++ 10_02.py
@@ -2,12 +2,11 @@ import itertools
 
 
 n = int(input("N: "))
-amount_of_files = 3
+
 
 with open("input.txt", "r", encoding="utf-8") as file:
     lines = [line.strip() for line in file]
 
-print(lines)
 
 for i in range(0, len(lines), n):
     with open(f"output{i/n+1}.txt", "w", encoding="utf-8") as file:
