21467f6c086d22cf208646db18e4051b194aac02
GatsuraOS <liolic_88@mail.ru>
2022-07-25 02:20:40 +0300

    create CRUDOrder on sqlqlchemy

--- CRUD/Order.py
+++ CRUD/Order.py
@@ -1,67 +1,60 @@
-import sqlite3
-from Schemas import OrderSchema
-from Schemas import OrderInDBSchema
+from sqlalchemy import delete, update, select
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+
+from Models import Order, create_session
+from Schemas import OrderSchema, OrderInDBSchema
 
 
-conn = sqlite3.connect("db.db")
-cur = conn.cursor()
-
-
-class OrderCRUD:
-
-    @staticmethod
-    def add(order: OrderSchema) -> None:
-        cur.execute("""
-        INSERT INTO orders(bot_user_id, date_create, status_id, invoice_id);
-        """, (order.bot_user_id, order.date_create, order.status_id, order.invoice_id))
-        conn.commit()
+class CRUDOrders:
 
     @staticmethod
-    def get(order_id: int) -> list[OrderInDBSchema]:
-        cur.execute("""
-        SELECT * FROM orders WHERE id = ?;
-        """, (order_id, ))
-        orders = []
-        for order in cur.fetchall():
-            orders.append(
-                OrderInDBSchema(
-                    id=order[0],
-                    bot_user_id=order[1],
-                    date_create=order[2],
-                    status_id=order[3],
-                    invoice_id=order[4]
-                )
+    @create_session
+    def add(order: OrderSchema, session: Session = None) -> OrderInDBSchema | None:
+        order = Order(
+            **order.__dict__
+        )
+        session.add(order)
+        try:
+            session.commit()
+        except IntegrityError:
+            return None
+        else:
+            session.refresh(order)
+            return OrderInDBSchema(**order.__dict__)
+
+    @staticmethod
+    @create_session
+    def get(order_id: int, session: Session = None) -> Order | None:
+        order = session.execute(
+            select(Order).where(Order.id == order_id)
+        )
+        order = order.first()
+        if order:
+            return order[0]
+
+    @staticmethod
+    @create_session
+    def get_all(session: Session = None) -> list[Order]:
+        orders = session.execute(
+            select(Order)
+        )
+        return [order[0] for order in orders.all()]
+
+    @staticmethod
+    @create_session
+    def delete(order_id: int, session: Session = None) -> None:
+        session.execute(
+            delete(Order).where(Order.id == order_id)
+        )
+        session.commit()
+
+    @staticmethod
+    @create_session
+    def update(order: OrderInDBSchema, session: Session = None) -> None:
+        session.execute(
+            update(Order).where(Order.id == order.id).values(
+                **order.__dict__
             )
-        return orders
-
-    @staticmethod
-    def get_all() -> list[OrderInDBSchema]:
-        cur.execute("""
-        SELECT * FROM orders;
-        """)
-        orders = []
-        for order in cur.fetchall():
-            orders.append(
-                OrderInDBSchema(
-                    id=order[0],
-                    bot_user_id=order[1],
-                    date_create=order[2],
-                    status_id=order[3],
-                    invoice_id=order[4]
-                )
-            )
-        return orders
-
-    @staticmethod
-    def update(order_id: int, order: OrderSchema) -> None:
-        cur.execute("""
-        UPDATE orders SET (bot_user_id, date_create, status_id, invoice_id) WHERE id = ?;
-        """, (order.bot_user_id, order.date_create, order.status_id, order.invoice_id, order_id))
-        conn.commit()
-
-    @staticmethod
-    def delete(order_id: int) -> None:
-        cur.execute("""
-        DELETE FROM orders WHERE id = ?;
-        """, (order_id, ))
-        conn.commit()
+        )
+        session.commit()
