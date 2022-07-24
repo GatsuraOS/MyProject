80c7a897ce0dc28e00a525bcab491061136ea18d
GatsuraOS <liolic_88@mail.ru>
2022-07-25 01:39:48 +0300

    create CRUDBotUser on sqlqlchemy

--- CRUD/BotUser.py
+++ CRUD/BotUser.py
@@ -1,65 +1,60 @@
-import sqlite3
-from Schemas import BotUserSchema
-from Schemas import BotUserInDBSchema
+from sqlalchemy import delete, update, select
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+
+from Schemas import BotUserSchema, BotUserInDBSchema
+from Models import BotUser, create_session
 
 
-conn = sqlite3.connect("db.db")
-cur = conn.cursor()
-
-
-class BotUserCRUD:
-
-    @staticmethod
-    def add(bot_user: BotUserSchema) -> None:
-        cur.execute("""
-        INSERT INTO bot_users(is_blocked, balance, language_id);
-        """, (bot_user.is_blocked, bot_user.balance, bot_user.language_id))
-        conn.commit()
+class CRUDBotUser:
 
     @staticmethod
-    def get(bot_user_id: str) -> list[BotUserInDBSchema]:
-        cur.execute("""
-        SELECT * FROM bot_users WHERE id = ?;
-        """, (bot_user_id, ))
-        bot_users = []
-        for bot_user in cur.fetchall():
-            bot_users.append(
-                BotUserInDBSchema(
-                    id=bot_user[0],
-                    is_blocked=bot_user[1],
-                    balance=bot_user[2],
-                    language_id=bot_user[3]
-                )
+    @create_session
+    def add(bot_user: BotUserSchema, session: Session = None) -> BotUserInDBSchema | None:
+        bot_user = BotUser(
+            **bot_user.__dict__
+        )
+        session.add(bot_user)
+        try:
+            session.commit()
+        except IntegrityError:
+            return None
+        else:
+            session.refresh(bot_user)
+            return BotUserInDBSchema(**bot_user.__dict__)
+
+    @staticmethod
+    @create_session
+    def get(bot_user_id: str, session: Session = None) -> BotUser | None:
+        bot_user = session.execute(
+            select(BotUser).where(BotUser.id == bot_user_id)
+        )
+        bot_user = bot_user.first()
+        if bot_user:
+            return bot_user[0]
+
+    @staticmethod
+    @create_session
+    def get_all(session: Session = None) -> list[BotUser]:
+        bot_users = session.execute(
+            select(BotUser)
+        )
+        return [bot_user[0] for bot_user in bot_users.all()]
+
+    @staticmethod
+    @create_session
+    def delete(bot_user: str, session: Session = None) -> None:
+        session.execute(
+            delete(BotUser).where(BotUser.id == bot_user)
+        )
+        session.commit()
+
+    @staticmethod
+    @create_session
+    def update(bot_user: BotUserInDBSchema, session: Session = None) -> None:
+        session.execute(
+            update(BotUser).where(BotUser.id == bot_user.id).values(
+                **bot_user.__dict__
             )
-        return bot_users
-
-    @staticmethod
-    def get_all() -> list[BotUserInDBSchema]:
-        cur.execute("""
-        SELECT * FROM bot_users;
-        """)
-        bot_users = []
-        for bot_user in cur.fetchall():
-            bot_users.append(
-                BotUserInDBSchema(
-                    id=bot_user[0],
-                    is_blocked=bot_user[1],
-                    balance=bot_user[2],
-                    language_id=bot_user[3]
-                )
-            )
-        return bot_users
-
-    @staticmethod
-    def update(bot_user_id: str, bot_user: BotUserSchema) -> None:
-        cur.execute("""
-        UPDATE bot_users SET (is_blocked, balance, language_id) WHERE id = ?;
-        """, (bot_user.is_blocked, bot_user.balance, bot_user.language_id, bot_user_id))
-        conn.commit()
-
-    @staticmethod
-    def delete(bot_user_id: str) -> None:
-        cur.execute("""
-        DELETE FROM bot_users WHERE id = ?;
-        """, (bot_user_id, ))
-        conn.commit()
+        )
+        session.commit()
