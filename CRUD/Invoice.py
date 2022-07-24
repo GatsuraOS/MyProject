13526cf13359c24f8d0e7d4617ebfba9347f3941
GatsuraOS <liolic_88@mail.ru>
2022-07-25 02:00:44 +0300

    create CRUDInvoice on sqlqlchemy

--- CRUD/Invoice.py
+++ CRUD/Invoice.py
@@ -1,67 +1,60 @@
-import sqlite3
-from Schemas import InvoiceSchema
-from Schemas import InvoiceInDBSchema
+from sqlalchemy import delete, update, select
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+
+from Models import Invoice, create_session
+from Schemas import InvoiceSchema, InvoiceInDBSchema
 
 
-conn = sqlite3.connect("db.db")
-cur = conn.cursor()
-
-
-class InvoiceCRUD:
-
-    @staticmethod
-    def add(invoice: InvoiceSchema) -> None:
-        cur.execute("""
-        INSERT INTO invoices(bot_user_id, date_create, total, status_id);
-        """, (invoice.bot_user_id, invoice.date_create, invoice.total, invoice.status_id))
-        conn.commit()
+class CRUDInvoice:
 
     @staticmethod
-    def get(invoice_id: str) -> list[InvoiceInDBSchema]:
-        cur.execute("""
-        SELECT * FROM invoices WHERE id = ?;
-        """, (invoice_id, ))
-        invoices = []
-        for invoice in cur.fetchall():
-            invoices.append(
-                InvoiceInDBSchema(
-                    id=invoice[0],
-                    bot_user_id=invoice[1],
-                    date_create=invoice[2],
-                    total=invoice[3],
-                    status_id=invoice[4]
-                )
+    @create_session
+    def add(invoice: InvoiceSchema, session: Session) -> InvoiceInDBSchema | None:
+        invoice = Invoice(
+            **invoice.__dict__
+        )
+        session.add(invoice)
+        try:
+            session.commit()
+        except IntegrityError:
+            return None
+        else:
+            session.refresh(invoice)
+            return InvoiceInDBSchema(**invoice.__dict__)
+
+    @staticmethod
+    @create_session
+    def get(invoice_id: str, session: Session = None) -> Invoice | None:
+        invoice = session.execute(
+            select(Invoice).where(Invoice.id == invoice_id)
+        )
+        invoice = invoice.first()
+        if invoice:
+            return invoice[0]
+
+    @staticmethod
+    @create_session
+    def get_all(session: Session = None) -> list[Invoice]:
+        invoices = session.execute(
+            select(Invoice)
+        )
+        return [invoice[0] for invoice in invoices.all()]
+
+    @staticmethod
+    @create_session
+    def delete(invoice_id: str, session: Session = None) -> None:
+        session.execute(
+            delete(Invoice).where(Invoice.id == invoice_id)
+        )
+        session.commit()
+
+    @staticmethod
+    @create_session
+    def update(invoice: InvoiceInDBSchema, session: Session = None) -> None:
+        session.execute(
+            update(Invoice).where(Invoice.id == invoice.id).values(
+                **invoice.__dict__
             )
-        return invoices
-
-    @staticmethod
-    def get_all() -> list[InvoiceInDBSchema]:
-        cur.execute("""
-        SELECT * FROM invoices;
-        """)
-        invoices = []
-        for invoice in cur.fetchall():
-            invoices.append(
-                InvoiceInDBSchema(
-                    id=invoice[0],
-                    bot_user_id=invoice[1],
-                    date_create=invoice[2],
-                    total=invoice[3],
-                    status_id=invoice[4]
-                )
-            )
-        return invoices
-
-    @staticmethod
-    def update(invoice_id: str, invoice: InvoiceSchema) -> None:
-        cur.execute("""
-        UPDATE invoices SET (bot_user_id, date_create, total, status_id) WHERE id = ?;
-        """, (invoice.bot_user_id, invoice.date_create, invoice.total, invoice.status_id, invoice_id))
-        conn.commit()
-
-    @staticmethod
-    def delete(invoice_id: int) -> None:
-        cur.execute("""
-        DELETE FROM invoices WHERE id = ?;
-        """, (invoice_id, ))
-        conn.commit()
+        )
+        session.commit()
